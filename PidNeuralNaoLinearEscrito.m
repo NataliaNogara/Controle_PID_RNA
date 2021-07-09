bb2474a16d8bf904c957d7b5c2b0cb244df4d99f
NataliaNogara <natalianogarastabile@gmail.com>
2021-07-09 10:39:36 -0300

    Primeiro commit

---
+++ PidNeuralNaoLinearEscrito.m
@@ -0,0 +1,119 @@
+clear all
+close all
+
+%Inicialização dos parâmetros
+N=250; % número de iterações
+Amax=1;
+y=zeros(1,N); % saída da planta
+u=zeros(1,N); % sinal de controle
+e=zeros(1,N); % erro
+w1=ones(1,N); % peso proporcional
+w2=ones(1,N); % peso integral
+w3=ones(1,N); % peso derivativo
+I=zeros(1,N);
+phiI=zeros(1,N);
+deltaw1=zeros(1,N);
+deltaw2=zeros(1,N);
+deltaw3=zeros(1,N);
+w1linha=zeros(1,N);
+w2linha=zeros(1,N);
+w3linha=zeros(1,N);
+
+% Parâmetros de ajuste da parte P
+eta1=0.1;
+w1=w1*5.7;
+
+% Parâmetros de ajuste da parte I
+eta2=0.1;
+w2=w2*0.1;
+
+% Parâmetros de ajuste a parte D
+eta3=0.1;
+w3=w3*0.1;
+
+% PID baseado em uma Rede Neural Artificial 
+for k=3:N
+r(k)=1.0; % sinal de referência
+% eq. de dif. da planta
+y(k)=0.9722*y(k-1)+0.3578*u(k-1)
+-0.1295*u(k-2)+0.3084*y(k-1)*y(k-2)*u(k-2)
+-0.04228*y(k-2)^2+0.1633*y(k-2)*u(k-2)
+-0.03259*y(k-2)*y(k-1)^2-0.3513*u(k-2)*y(k-1)^2
+-0.3103*y(k-1)*u(k-1)+0.1087*y(k-2)*u(k-1)*u(k-2);
+e(k)=r(k)-y(k); % sinal do erro
+
+x1(k)=e(k)-e(k-1); 
+x2(k)=e(k);
+x3(k)=e(k)-2*e(k-1)+e(k-2);
+
+I(k)=x1(k)*w1(k)+x2(k)*w2(k)+x3(k)*w3(k);
+phiI(k)=(Amax*(1-exp(-I(k))))/(1+exp(-I(k)));
+u(k)=u(k-1)+phiI(k);    
+
+deltaw1(k)=eta1*x1(k)*e(k)*u(k); 
+deltaw2(k)=eta2*x2(k)*e(k)*u(k);
+deltaw3(k)=eta3*x3(k)*e(k)*u(k);
+
+w1linha(k)=w1(k-1)+deltaw1(k); 
+w2linha(k)=w2(k-1)+deltaw2(k); 
+w3linha(k)=w3(k-1)+deltaw3(k); 
+
+w1(k)=w1linha(k)/sqrt(w1linha(k)^2+w2linha(k)^2+w3linha(k)^2); % Kp
+w2(k)=w2linha(k)/sqrt(w1linha(k)^2+w2linha(k)^2+w3linha(k)^2); % Ki
+w3(k)=w3linha(k)/sqrt(w1linha(k)^2+w2linha(k)^2+w3linha(k)^2); % Kd 
+
+I(k)=x1(k)*w1(k)+x2(k)*w2(k)+x3(k)*w3(k);
+phiI(k)=(Amax*(1-exp(-I(k))))/(1+exp(-I(k)));
+u(k)=u(k-1)+phiI(k);
+end
+
+% Plotar gráficos
+k=0:N-1;
+
+figure(1)
+plot(k,r,'-.',k,y,'-')
+legend('r(k)','y(k)');
+title ('Saída do sistema com PID neural');
+xlabel('kT');
+ylabel('Saída y(k)');
+grid on;
+
+figure(2)
+plot(k,u,'-');
+legend('u(k)');
+title ('Ação de controle do sistema com PID neural');
+xlabel('kT');
+ylabel('Sinal de controle u(k)');
+grid on;
+
+figure(3)
+plot(k,w1,k,w2,k,w3)
+legend('w1','w2','w3');
+title ('Convergência dos pesos')
+xlabel('kT');
+ylabel('Amplitude');
+grid on;
+
+u=[0*ones(1,2) 1*ones(1,N-2)];
+r=u;
+N=length(u);
+
+y(1)=0.0;
+y(2)=0.0;
+
+for k=3:N
+y(k)=0.9722*y(k-1)+0.3578*u(k-1)
+-0.1295*u(k-2)+0.3084*y(k-1)*y(k-2)*u(k-2)
+-0.04228*y(k-2)^2+0.1633*y(k-2)*u(k-2)
+-0.03259*y(k-2)*y(k-1)^2-0.3513*u(k-2)*y(k-1)^2
+-0.3103*y(k-1)*u(k-1)+0.1087*y(k-2)*u(k-1)*u(k-2);
+end
+
+k=0:N-1;
+figure(4)
+plot(k,r,'-.',k,y,'-');
+legend('r(k)','y(k)');
+title ('Saída do sistema em malha aberta');
+xlabel('kT');
+ylabel('Saída y(k)');
+grid on;
---
+++ README.md
@@ -0,0 +1,27 @@
+# Controle PID baseado em neurônio artificial
+
+
+
+​    Este trabalho de conclusão de curso apresenta a simulação do controle de dois sistemas
+lineares, sendo um de primeira e outro de segunda ordem e o controle de um sistema não linear
+de segunda ordem, utilizando um controlador PID. Os parâmetros do PID foram sintonizados
+por meio dos pesos sinápticos de uma rede neural artificial composta por um único neurônio.
+
+​    Essa escolha se deve à simplicidade e boa performance dos controladores PID e à facilidade
+computacional e de manipulação de não linearidades que tal rede propicia. Para a simulação
+do controlador, foi utilizado um algoritmo composto pela Regra de Hebb supervisionada e pela
+Regra de Hebb não supervisionada. Os resultados das simulações mostram que o controlador
+PID neural apresentou um ótimo desempenho para os três sistemas simulados.
+
+
+
+# PID based on artificial neuron
+
+
+
+​    This course conclusion paper presents the simulation of the control of two linear systems, being
+one of first order and the other of second order, and the control of a nonlinear system of second
+order, using a PID controller. The PID parameters are tuned using the synaptic weights of an
+artificial neural network composed of a single neuron. 
+
+​    This choice is due to the simplicity and good performance of the PID controllers and the computational and nonlinearity manipulation facilities that this network provides. For the controller simulation, an algorithm composed of the supervised Hebb Rule and the unsupervised Hebb Rule was used. The results of the simulations show that the neural PID controller presented an excellent performance for the three simulated systems.
